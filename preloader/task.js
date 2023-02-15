f43016de7f4e2298247078b273eca90c604525cc
LukLena <106184354+LukLena@users.noreply.github.com>
2023-02-15 20:00:35 +0300

    second

--- preloader/task.js
+++ preloader/task.js
@@ -1,26 +1,29 @@
 
 const img = document.getElementById('loader')
-const items = document.getElementsByClassName('items')
+const items = document.getElementById('items')
 console.log(img)
 let xhr = new XMLHttpRequest()
 xhr.open('GET', `https://students.netoservices.ru/nestjs-backend/slow-get-courses`)
 xhr.send()
 
 
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 ){
-       img.classList.remove('loader_active')
-    
-       // console.log(xhr.responseText)
-//        const dollar = JSON.parse(xhr.responseText)
-//        // массив валют 
-//        const valute = dollar.response['Valute']
-//        console.log(valute)
-//        // для каждого элемента валют создавать див с классом item
-////        const itemCode 
-//        valute.forEach( (el) => {
-//           items[0].insertAdjacentHTML('BeforeAfter', `<div class="item"></div>`) 
-//        })
-        
-        }
+xhr.onload = function(){
+    img.classList.remove('loader_active')
+    const dollar = JSON.parse(xhr.responseText)
+    const valute = dollar.response['Valute']
+    for (let key in valute){
+         const charCode = dollar.response.Valute[key].CharCode
+         const value = dollar.response.Valute[key].Value
+         items.insertAdjacentHTML('BeforeEnd', `<div class="item">
+                <div class="item__code">
+                    ${charCode}
+                </div>
+                <div class="item__value">
+                    ${value}
+                </div>
+                <div class="item__currency">
+                    руб.
+                </div>
+          </div>`)
+    }        
 }
